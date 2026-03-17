4f2d36ccf7211a59d075c2285903377ad617eaad
ziv ankri <za6200@bs.amalnet.k12.il>
2026-03-18 07:05:41 +1400

    working on side bar

--- SpotiPsi-14-43/src/components/MainSection/MainSection.tsx
+++ SpotiPsi-14-43/src/components/MainSection/MainSection.tsx
@@ -1,5 +1,5 @@
 import useStyles from "./MainSectionStyles";
-{/*import Menu from "./MainSection/SideBar/SideBar"*/}
+import Menu from "./SideBar/SideBar.tsx"
 import Content from "./MainSection/Pages/AllSongs/AllSongs.tsx"
 
 const MainSection: React.FC = () => {
@@ -7,9 +7,8 @@ const MainSection: React.FC = () => {
 
   return (
   <div className={classes.container}>
-    <h1>no!!!!!!</h1>
     <Content />
-    {/* <Menu/> */}
+    <Menu />
     
   </div>
   )
