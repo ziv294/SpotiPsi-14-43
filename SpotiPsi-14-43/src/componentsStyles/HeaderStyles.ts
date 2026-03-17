9c5ea59350ef979c0b8ba9ba7262ec9247dc97ec
taizerflash <lioruziel2006@gmail.com>
2026-03-17 17:04:59 +0200

    header created

---
+++ SpotiPsi-14-43/src/componentsStyles/HeaderStyles.ts
@@ -0,0 +1,18 @@
+import {makeStyles} from "@mui/styles";
+
+const useStyles=makeStyles({
+  header:{
+    width: "100%",
+    display: "flex",
+    justifyContent: "start",
+    alignItems: "right",
+    marginBottom: "2rem",
+    color: "rgb(173, 73, 212)",
+    backgroundColor:"rgb(53, 52, 52)",
+  },
+  headerText:{
+    marginRight: "1rem"
+  }
+});
+
+export default useStyles;
