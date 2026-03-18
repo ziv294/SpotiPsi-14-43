15a373c6853bc397f8151b5f03d080b0a74ba218
ziv ankri <za6200@bs.amalnet.k12.il>
2026-03-18 21:05:23 +1400

    finished sidebar

--- SpotiPsi-14-43/src/components/MainSection/SideBar/SideBar.tsx
+++ SpotiPsi-14-43/src/components/MainSection/SideBar/SideBar.tsx
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 //import { useNavigate } from "react-router-dom";
+import { BsHouseFill, BsFileEarmarkPlusFill, BsHeartFill } from "react-icons/bs";
 import "./SideBar.css";
 
 const SideBar: React.FC = () => {
   //const navigate = useNavigate();
+  const [selected, setSelected] = useState<string>("songs");
+
+  const handleClick = (name: string, path: string) => {
+    setSelected(name);
+    //navigate(path);
+  };
 
   return (
     <div className="sidebar">
-      <div className="item active" onClick={() => null}>
-        🏠︎ כל השירים
+      <div className={selected === "songs" ? "item active" : "item"} onClick={() => handleClick("songs", "/")}>
+        <BsHouseFill /> כל השירים 
       </div>
 
-      <div className="item" onClick={() => null}>
-        פלייליסטים
+      <div className={selected === "playlists" ? "item active" : "item"} onClick={() => handleClick("playlists", "/playlists")}>
+         <BsFileEarmarkPlusFill /> פלייליסטים 
       </div>
 
-      <div className="item" onClick={() => null}>
-        מועדפים
+      <div className={selected === "favorites" ? "item active" : "item"} onClick={() => handleClick("favorites", "/favorites")}>
+         <BsHeartFill /> מועדפים 
       </div>
     </div>
   );
