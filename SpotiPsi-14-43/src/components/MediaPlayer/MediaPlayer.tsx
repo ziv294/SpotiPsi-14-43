f0a567970cf16a76ae1c9ace5f27a272b2a47711
ziv ankri <za6200@bs.amalnet.k12.il>
2026-03-19 07:04:17 +1400

    finished playing songs

--- SpotiPsi-14-43/src/components/MediaPlayer/MediaPlayer.tsx
+++ SpotiPsi-14-43/src/components/MediaPlayer/MediaPlayer.tsx
@@ -1,89 +1,67 @@
-import React, { useEffect, useState } from "react";
-import { FaPlay, FaPause, FaStepBackward, FaStepForward } from "react-icons/fa";
+import React, { useEffect } from "react";
 import "./MediaPlayer.css";
-
-type Song = {
-  name: string;
-  artist: string;
-  duration: number;
-};
-
-const songs: Song[] = [
-  { name: "song 1", artist: "artist1", duration: 247 },
-  { name: "song 2", artist: "artist2", duration: 200 },
-  { name: "song 3", artist: "artist3", duration: 203 }
-];
+import useAudioPlayer from "../CustomHooks/useAudioPlayer";
+import { FaPlay, FaPause, FaStepBackward, FaStepForward } from "react-icons/fa";
 
 const MediaPlayer: React.FC = () => {
-  const [currentSongIndex, setCurrentSongIndex] = useState<number>(0);
-  const [isPlaying, setIsPlaying] = useState<boolean>(false);
-  const [currentTime, setCurrentTime] = useState<number>(0);
-
-  const currentSong = songs[currentSongIndex];
+  const {
+    currentSong,
+    isPlaying,
+    currentTime,
+    duration,
+    fetchSongs,
+    audioRef,
+    setCurrentTime,
+    setDuration,
+    setIsPlaying,
+    handlePlayPause,
+    handleNext,
+    handlePrev,
+    formatTime
+  } = useAudioPlayer();
 
   useEffect(() => {
-    if (!isPlaying){
-         return;
-    }
+    fetchSongs();
+  }, []);
 
-    const timer = setInterval(() => {
-      setCurrentTime(prev => prev + 1);
-    }, 1000);
-
-    return () => clearInterval(timer);
-  }, [isPlaying, currentSong]);
-
-  const handlePlayPause = (): void => {
-    setIsPlaying(!isPlaying);
-  };
-
-  const handleNext = (): void => {
-    
-    setCurrentSongIndex(prev => prev + 1 === songs.length ? prev = 0: prev = prev + 1); // make it in circle
-    setCurrentTime(0);
-    setIsPlaying(false);
-  };
-
-  const handlePrev = (): void => {
-    setCurrentTime(0);
-    setIsPlaying(false);
-    setCurrentSongIndex(prev => prev - 1 === -1 ? prev = songs.length - 1: prev = prev - 1); // make it in circle
-  };
-
-  const formatTime = (time: number): string => {
-    const minutes = Math.floor(time / 60); // removes decimal of the minute
-    const seconds = time % 60;
-    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-  };
+  const songPath = currentSong ? `/songs/${currentSong.id}.mp3` : "";
 
   return (
     <div className="MediaPlayer">
       <div className="songInfo">
-        <h2 className="songName">{currentSong.name}</h2>
-        <p className="artistName">{currentSong.artist}</p>
+        <h2 className="songName">{currentSong?.name ?? "No song selected"}</h2>
+        <p className="artistName">{currentSong?.artist ?? ""}</p>
       </div>
 
+      <audio key={currentSong?.id} ref={audioRef} src={songPath} onTimeUpdate={(e) => setCurrentTime(e.currentTarget.currentTime)}
+onLoadedMetadata={(e) => setDuration(Math.floor(e.currentTarget.duration))}
+onEnded={() => {
+    setIsPlaying(false);
+    handleNext();
+        }}
+      />
+
       <div className="controls">
-        
-        <button className="controlButton" onClick={handleNext}>
+        <button className="controlButton" onClick={handlePrev}>
           <FaStepForward />
+          
         </button>
 
         <button className="playButton" onClick={handlePlayPause}>
           {isPlaying ? <FaPause /> : <FaPlay />}
         </button>
 
-        <button className="controlButton" onClick={handlePrev}>
+        <button className="controlButton" onClick={handleNext}>
           <FaStepBackward />
         </button>
       </div>
 
       <div className="timeBarSection">
-        <span className="timeText">{formatTime(currentTime)}</span>
+        <div className="timeText">{formatTime(currentTime)}</div>
 
-        <input className="timeBar" type="range" min="0" max={currentSong.duration} value={currentTime}/>
+        <input className="timeBar" type="range" min="0" max={duration} value={currentTime} readOnly/>  
 
-        <span className="timeText">{formatTime(currentSong.duration)}</span>
+        <div className="timeText">{formatTime(duration)}</div>
       </div>
     </div>
   );
