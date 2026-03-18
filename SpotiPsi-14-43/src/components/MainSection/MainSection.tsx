f6b3b308e05ab8af1c065dcfe33b799f008299a1
ziv ankri <za6200@bs.amalnet.k12.il>
2026-03-18 22:01:27 +1400

    fixed routing

--- SpotiPsi-14-43/src/components/MainSection/MainSection.tsx
+++ SpotiPsi-14-43/src/components/MainSection/MainSection.tsx
@@ -1,6 +1,32 @@
 import useStyles from "./MainSectionStyles";
 import Menu from "./SideBar/SideBar.tsx"
 import Content from "./MainSection/Pages/AllSongs/AllSongs.tsx"
+import type React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+
+const AllSongsPage: React.FC = () => {
+  return (
+    <div>
+      <Content />
+    </div>
+  )
+}
+
+const PlaylistsPage: React.FC = () => {
+  return (
+    <div>
+      <Content />
+    </div>
+  )
+}
+
+const FavoritesPage: React.FC = () => {
+  return (
+    <div>
+      <Content />
+    </div>
+  )
+}
 
 const MainSection: React.FC = () => {
   const classes = useStyles();
@@ -9,7 +35,13 @@ const MainSection: React.FC = () => {
   <div className={classes.container}>
     <Menu />
     <div className={classes.content}>
-        <Content />
+        <Router>
+          <Routes>
+            <Route path="/" element={<AllSongsPage/>}></Route>
+            <Route path="/playlists" element={<PlaylistsPage/>}></Route>
+            <Route path="/favorites" element={<FavoritesPage/>}></Route>
+          </Routes>
+        </Router>
     </div>
     
     
