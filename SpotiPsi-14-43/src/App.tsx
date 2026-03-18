ba3db854a01eb351cfac09935f4c8d078ba7147d
ziv ankri <za6200@bs.amalnet.k12.il>
2026-03-19 02:19:05 +1400

    finished media player befor connecting to server

--- SpotiPsi-14-43/src/App.tsx
+++ SpotiPsi-14-43/src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header/Header"
 import MainSection from './components/MainSection/MainSection.tsx'
 import './App.css'
+import MediaPlayer from "./components/MediaPlayer/MediaPlayer.tsx"
 
 function App() {
   return (
     <>
       <Header />
       <MainSection/>
+      <MediaPlayer />
     </>
   )
 }
