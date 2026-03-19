0ca04a5f6af611e6d7e3eed597fc0ffc77a6003b
taizerflash <lioruziel2006@gmail.com>
2026-03-19 09:27:06 +0200

    adding songs logic and back btn

--- SpotiPsi-14-43/src/components/MainSection/MainSection/Pages/Platlists/Playlist/Playlist.tsx
+++ SpotiPsi-14-43/src/components/MainSection/MainSection/Pages/Platlists/Playlist/Playlist.tsx
@@ -1,84 +1,187 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import useStyles from "../../AllSongs/AllSongs";
+import { useNavigate } from "react-router-dom";
 
 type Song = {
-  id: string;
-  name: string;
-  artist: string;
+    id: string;
+    name: string;
+    artist: string;
 };
 
 type Playlist = {
-  id: string;
-  name: string;
-  songIds: string[];
+    id: string;
+    name: string;
+    songIds: string[];
 };
 
 const PlaylistPage: React.FC = () => {
-  const classes = useStyles();
-  const { playlistId } = useParams();
+    const classes = useStyles();
+    const navigate = useNavigate();
+    const { playlistId } = useParams();
 
-  const [songs, setSongs] = useState<Song[]>([]);
-  const [playlist, setPlaylist] = useState<Playlist | null>(null);
+    const [songs, setSongs] = useState<Song[]>([]);
+    const [playlist, setPlaylist] = useState<Playlist | null>(null);
 
-  const fetchSongs = async () => {
-    try {
-      const res = await fetch("http://localhost:5001/api/songs");
-      const data = await res.json();
-      setSongs(data);
-    } catch (err) {
-      console.error(err);
-    }
-  };
+    const [favorites, setFavorites] = useState<string[]>([]);
+    const [playlists, setPlaylists] = useState<Playlist[]>([]);
+    const [openMenuId, setOpenMenuId] = useState<string | null>(null);
 
-  const fetchPlaylist = async () => {
-    try {
-      const res = await fetch("http://localhost:5001/api/playlists");
-      const data = await res.json();
+    const fetchSongs = async () => {
+        try {
+            const res = await fetch("http://localhost:5001/api/songs");
+            const data = await res.json();
+            setSongs(data);
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-      const current = data.find((p: Playlist) => p.id === playlistId);
-      setPlaylist(current || null);
+    const fetchFavorites = async () => {
+        try {
+            const res = await fetch("http://localhost:5001/api/favorites");
+            const data = await res.json();
+            setFavorites(data);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    };
 
-    } catch (err) {
-      console.error(err);
-    }
-  };
+    const fetchPlaylists = async () => {
+        try {
+            const res = await fetch("http://localhost:5001/api/playlists");
+            const data = await res.json();
+            setPlaylists(data);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    };
 
-  useEffect(() => {
-    fetchSongs();
-    fetchPlaylist();
-  }, [playlistId]);
+    const fetchPlaylist = async () => {
+        try {
+            const res = await fetch("http://localhost:5001/api/playlists");
+            const data = await res.json();
 
-  const playlistSongs = songs.filter(song =>
-    playlist?.songIds.includes(song.id)
-  );
+            const current = data.find((p: Playlist) => p.id === playlistId);
+            setPlaylist(current || null);
 
-  return (
-    <div className={classes.songsContainer}>
-        <div className={classes.header}>
-            <h1 className={classes.title}>
-                {`פלייליסט ${playlist?.name}`}
-            </h1>
-            <h1 > ⇦</h1>
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    useEffect(() => {
+        fetchSongs();
+        fetchFavorites();
+        fetchPlaylists();
+        fetchPlaylist();
+    }, [playlistId]);
+
+    const addToPlaylist = async (playlistId: string, songId: string) => {
+        try {
+            await fetch(`http://localhost:5001/api/playlists/${playlistId}/add`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ playlistId, songId }),
+            });
+            setOpenMenuId(null);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    };
+
+    const toggleFavorite = async (id: string) => {
+        try {
+            const isFavorite = favorites.includes(id);
+            const url = isFavorite
+                ? "http://localhost:5001/api/favorites/remove"
+                : "http://localhost:5001/api/favorites/add";
+
+            const res = await fetch(url, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ songId: id }),
+            });
+            const updatedFavorites = await res.json();
+            setFavorites(updatedFavorites);
+        }
+        catch (err) {
+            console.error(err);
+        }
+    };
+
+    const playlistSongs = songs.filter(song =>
+        playlist?.songIds.includes(song.id)
+    );
+
+    return (
+        <div className={classes.songsContainer}>
+            <div className={classes.header}>
+                <h1 className={classes.title}>
+                    פלייליסט {playlist?.name}
+                </h1>
+                <h1 
+                    className={classes.backBtn}
+                    onClick={() => navigate(`/playlists`)}>⇦
+                </h1>
+            </div>
+
+            <div className={classes.songsList}>
+                {playlistSongs.map(song => {
+                    const isFav = favorites.includes(song.id);
+
+                    return (
+                        <div key={song.id} className={classes.songRow}>
+                            <div className={classes.left}>
+                                <div className={classes.play}>▶</div>
+                                {song.name} - {song.artist}
+                            </div>
+
+                            <div className={classes.right}>
+                                <div style={{ position: "relative" }}>
+                                    <span
+                                        onClick={() =>
+                                            setOpenMenuId(openMenuId === song.id ? null : song.id)
+                                        }
+                                    >
+                                        ✚
+                                    </span>
+
+                                    {openMenuId === song.id && (
+                                        <div className={classes.dropDown}>
+                                            {playlists.map(p => (
+                                                <div
+                                                    key={p.id}
+                                                    className={classes.dropDownItem}
+                                                    onClick={() => addToPlaylist(p.id, song.id)}
+                                                >
+                                                    {p.name}
+                                                </div>
+                                            ))}
+                                        </div>
+                                    )}
+                                </div>
+
+                                <span
+                                    onClick={() => toggleFavorite(song.id)}
+                                    className={`${classes.heart} ${isFav ? classes.activeHeart : ""}`}
+                                >
+                                    ❤︎
+                                </span>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
-      
-
-      <div className={classes.songsList}>
-
-        {playlistSongs.map(song => (
-          <div key={song.id} className={classes.songRow}>
-            <div className={classes.left}>
-              <div className={classes.play}>▶</div>
-              {song.name} - {song.artist}
-            </div>
-
-            <div className={classes.right}>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+    );
 };
 
 export default PlaylistPage;
